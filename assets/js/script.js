03f383cd12cba0d11e626fb360fabe810bde9154
Niraja85 <tripathi.niraja@gmail.com>
2025-03-11 22:36:51 +0100

    Added respective headings to all the images ofvrock,paper,scissors.lozard and spock

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,32 @@
 // Cached the DOM so that all the variables are declared
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result");
 const rock_div = document.getElementById("rk");
 const paper_div = document.getElementById("pr");
 const scissors_div = document.getElementById("sr");
 const lizard_div = document.getElementById("lz");
 const spock_div = document.getElementById("sp");
 
+// This function gives a random computer choice.
 function getComputerChoice(){
     const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
     const randomNumber = (Math.floor(Math.random() * 5));
     return choices[randomNumber];   
 }
+function Win(){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+}
+
+  //add result message  
+
+
+
 function game(playerChoice){
     const computerChoice = getComputerChoice();
 switch (playerChoice + computerChoice){
@@ -29,7 +40,7 @@ switch (playerChoice + computerChoice){
     case "lizardspock":
     case "spockscissors":
     case "spockrock":
-        console.log("YOU WIN.")
+        Win();
     break;
     case "scissorsrock":
     case "lizardrock":
@@ -41,14 +52,14 @@ switch (playerChoice + computerChoice){
     case "spocklizard":
     case "scissorsspock":
     case "rockspock":
-        console.log("YOU LOSE.");
+        lose();
     break;
     case "rockrock":
     case "paperpaper":
     case "scissorsscissors":
     case "lizardlizard":
     case "spockspock":
-        console.log("IT'S A DRAW.");
+        draw();
     break;                                                           
 
 }
