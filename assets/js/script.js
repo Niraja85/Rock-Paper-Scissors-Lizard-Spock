45e5340c4cda376b19bfac35bef51c40aca10877
Niraja85 <tripathi.niraja@gmail.com>
2025-03-12 01:26:10 +0100

    Added message to be displayed for winning, losing and draw

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_p = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("rk");
 const paper_div = document.getElementById("pr");
 const scissors_div = document.getElementById("sr");
@@ -13,75 +13,86 @@ const spock_div = document.getElementById("sp");
 
 // This function gives a random computer choice.
 function getComputerChoice(){
-    const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+    const choices = ['ROCK', 'PAPER', 'SCISSORS', 'LIZARD', 'SPOCK'];
     const randomNumber = (Math.floor(Math.random() * 5));
     return choices[randomNumber];   
 }
-function Win(){
+function Win(playerChoice, computerChoice){
     userScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
+    const playerWord = "player".fontsize(3);
+    result_p.innerHTML = `${(playerChoice)}(player) beats ${(computerChoice)}(comp). You WON !!`;
+}
+function Lose(playerChoice, computerChoice){
+  computerScore++;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${(playerChoice)} loses to ${(computerChoice)}. You LOST !!`;
 }
 
-  //add result message  
+function Draw(playerChoice, computerChoice){
+  result_p.innerHTML = `${(playerChoice)} and  ${(computerChoice)}. Its a DRAW!!`;
+
+}
+
+ 
 
 
 
 function game(playerChoice){
     const computerChoice = getComputerChoice();
 switch (playerChoice + computerChoice){
-    case "rockscissors":
-    case "rocklizard":
-    case "paperrock":
-    case "paperspock":
-    case "scissorspaper":
-    case "scissorslizard":
-    case "lizardpaper":
-    case "lizardspock":
-    case "spockscissors":
-    case "spockrock":
-        Win();
+    case "ROCKSCISSORS":
+    case "ROCKLIZARD":
+    case "PAPERROCK":
+    case "PAPERSPOCK":
+    case "SCISSORSPAPER":
+    case "SCISSORSLIZARD":
+    case "LIZARDPAPER":
+    case "LIZARDSPOCK":
+    case "SPOCKSCISSORS":
+    case "SPOCKROCK":
+        Win(playerChoice, computerChoice);
     break;
-    case "scissorsrock":
-    case "lizardrock":
-    case "rockpaper":
-    case "spockpaper":
-    case "paperscissors":
-    case "lizardscissors":
-    case "paperlizard":
-    case "spocklizard":
-    case "scissorsspock":
-    case "rockspock":
-        lose();
+    case "SCISSORSROCK":
+    case "LIZARDROCK":
+    case "ROCKPAPER":
+    case "SPOCKPAPER":
+    case "PAPERSCISSORS":
+    case "LIZARDSCISSORS":
+    case "PAPERLIZARD":
+    case "SPOCKLIZARD":
+    case "SCISSORSSPOCK":
+    case "ROCKSPOCK":
+        Lose(playerChoice, computerChoice);
     break;
-    case "rockrock":
-    case "paperpaper":
-    case "scissorsscissors":
-    case "lizardlizard":
-    case "spockspock":
-        draw();
+    case "ROCKROCK":
+    case "PAPERPAPER":
+    case "SCISSORSSCISSORS":
+    case "LIZARDLIZARD":
+    case "SPOCKSPOCK":
+        Draw(playerChoice, computerChoice);
     break;                                                           
 
 }
     
 }    
 
-
 function main(){
   rock_div.addEventListener('click' , function(){
-    game("rock");
+    game("ROCK");
    })
   paper_div.addEventListener('click' , function(){
-    game("paper");
+    game("PAPER");
    })
   scissors_div.addEventListener('click' , function(){
-    game("scissors");
+    game("SCISSORS");
    })
   lizard_div.addEventListener('click' , function(){
-    game("lizard");
+    game("LIZARD");
   })
   spock_div.addEventListener('click' , function(){
-    game("spock");
+    game("SPOCK");
   })
 }
 
